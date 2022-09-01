6ccc27499b82db71073f014623117b388d063907
Neil <neil@premiergsolutions.com>
2022-09-01 20:15:16 +0200

    Replaced array with HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const DIGIT0: &str = " _ | ||_|";
 const DIGIT1: &str = "     |  |";
 const DIGIT2: &str = " _  _||_ ";
@@ -54,6 +56,9 @@ fn main() {
     //       _  _     _  _  _  _  _
     //     | _| _||_||_ |_   ||_||_|
     //     ||_  _|  | _||_|  ||_| _|
+    let mut digits_hash = HashMap::new();
+
+    populate_digits(&mut digits_hash);
 
     let first_line = "    _  _     _  _  _  _  _ ";
     let second_line = "  | _| _||_||_ |_   ||_||_|";
@@ -61,13 +66,22 @@ fn main() {
 
     for i in 0..9 {
         let extracted_digit = extract_digit(first_line, second_line, third_line, i);
-        let detected_digit = detect_digit(extracted_digit);
+        let detected_digit = detect_digit(&digits_hash, extracted_digit);
         print!("{}", detected_digit);
     }
+}
 
-    // for digit in DIGITS {
-    //     print_digit(digit);
-    // }
+fn populate_digits(digits: &mut HashMap<&str, u32>) {
+    digits.insert(DIGIT0, 0);
+    digits.insert(DIGIT1, 1);
+    digits.insert(DIGIT2, 2);
+    digits.insert(DIGIT3, 3);
+    digits.insert(DIGIT4, 4);
+    digits.insert(DIGIT5, 5);
+    digits.insert(DIGIT6, 6);
+    digits.insert(DIGIT7, 7);
+    digits.insert(DIGIT8, 8);
+    digits.insert(DIGIT9, 9);
 }
 
 fn print_digit(digit: &str) {
@@ -88,13 +102,9 @@ fn extract_digit(line1: &str, line2: &str, line3: &str, index: usize) -> String 
     )
 }
 
-fn detect_digit(digit_to_detect: String) -> u8 {
-    let mut i = 0;
-    for digit in DIGITS {
-        if *digit == digit_to_detect {
-            return i;
-        }
-        i += 1;
+fn detect_digit(digits: &HashMap<&str, u32>, digit_to_detect: String) -> u8 {
+    match digits.get(digit_to_detect.as_str()) {
+        Some(index) => *index as u8,
+        None => 255,
     }
-    i
 }
