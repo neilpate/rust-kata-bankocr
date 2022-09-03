f1193e96845a6fc600e1593d55fa1fe20c5e54e8
Neil <neil@premiergsolutions.com>
2022-09-03 23:42:35 +0200

    feat: Moved implementation to a module

---
+++ src/account_manager.rs
@@ -0,0 +1,64 @@
+pub struct AccountNumber {
+    pub number: String,
+    line1: String,
+    line2: String,
+    line3: String,
+}
+
+impl AccountNumber {
+    pub fn new(line1_str: &str, line2_str: &str, line3_str: &str) -> Self {
+        let mut an = AccountNumber {
+            line1: line1_str.to_owned(),
+            line2: line2_str.to_owned(),
+            line3: line3_str.to_owned(),
+            number: "".to_owned(),
+        };
+
+        let detected_digits = (0..9)
+            .map(|i| detect_digit(extract_digit(line1_str, line2_str, line3_str, i)))
+            .collect::<Vec<u8>>();
+
+        let chars = detected_digits.iter().map(|d| d + 48).collect::<Vec<u8>>();
+
+        let x = String::from_utf8(chars);
+        an.number = x.unwrap();
+
+        an
+    }
+
+    pub fn print(&self) {
+        println!("{}", self.line1.trim_end());
+        println!("{}", self.line2.trim_end());
+        println!("{}", self.line3.trim_end());
+        println!("{}", self.number);
+    }
+}
+fn extract_digit(line1: &str, line2: &str, line3: &str, index: usize) -> String {
+    let start: usize = index * 3;
+    let stop = start + 3;
+
+    let composite_string = format!(
+        "{}{}{}",
+        &line1[start..stop],
+        &line2[start..stop],
+        &line3[start..stop]
+    );
+
+    composite_string
+}
+
+fn detect_digit(digit_to_detect: String) -> u8 {
+    match digit_to_detect.as_str() {
+        " _ | ||_|" => 0,
+        "     |  |" => 1,
+        " _  _||_ " => 2,
+        " _  _| _|" => 3,
+        "   |_|  |" => 4,
+        " _ |_  _|" => 5,
+        " _ |_ |_|" => 6,
+        " _   |  |" => 7,
+        " _ |_||_|" => 8,
+        " _ |_| _|" => 9,
+        _ => 255,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
+//use acc;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 use std::path::Path;
-use std::str;
 
-struct AccountNumber {
-    number: String,
-    line1: String,
-    line2: String,
-    line3: String,
-}
-
-impl AccountNumber {
-    fn new(line1_str: &str, line2_str: &str, line3_str: &str) -> Self {
-        AccountNumber {
-            line1: line1_str.to_owned(),
-            line2: line2_str.to_owned(),
-            line3: line3_str.to_owned(),
-            number: "".to_owned(),
-        }
-    }
-
-    fn print(&self) {
-        println!("{}", self.line1.trim_end());
-        println!("{}", self.line2.trim_end());
-        println!("{}", self.line3.trim_end());
-    }
-}
+mod account_manager;
 
 fn main() -> Result<(), Error> {
     let path = Path::new("resource\\accounts.txt");
@@ -52,7 +30,7 @@ fn main() -> Result<(), Error> {
 
 fn process_next_account_number(
     buffered_file: &mut BufReader<File>,
-) -> Result<AccountNumber, Error> {
+) -> Result<account_manager::AccountNumber, Error> {
     let mut line1 = String::new();
     let mut line2 = String::new();
     let mut line3 = String::new();
@@ -70,48 +48,8 @@ fn process_next_account_number(
         ));
     }
 
-    let mut account_number = AccountNumber::new(&first_line, &second_line, &third_line);
-
-    account_number.print();
-
-    let detected_digits = (0..9)
-        .map(|i| detect_digit(extract_digit(first_line, second_line, third_line, i)))
-        .collect::<Vec<u8>>();
-
-    let chars = detected_digits.iter().map(|d| d + 48).collect::<Vec<u8>>();
-
-    let x = String::from_utf8(chars);
-    account_number.number = x.unwrap();
+    let account_number =
+        account_manager::AccountNumber::new(&first_line, &second_line, &third_line);
 
     Ok(account_number)
-}
-
-fn extract_digit(line1: &str, line2: &str, line3: &str, index: usize) -> String {
-    let start: usize = index * 3;
-    let stop = start + 3;
-
-    let composite_string = format!(
-        "{}{}{}",
-        &line1[start..stop],
-        &line2[start..stop],
-        &line3[start..stop]
-    );
-
-    composite_string
-}
-
-fn detect_digit(digit_to_detect: String) -> u8 {
-    match digit_to_detect.as_str() {
-        " _ | ||_|" => 0,
-        "     |  |" => 1,
-        " _  _||_ " => 2,
-        " _  _| _|" => 3,
-        "   |_|  |" => 4,
-        " _ |_  _|" => 5,
-        " _ |_ |_|" => 6,
-        " _   |  |" => 7,
-        " _ |_||_|" => 8,
-        " _ |_| _|" => 9,
-        _ => 255,
-    }
 }
